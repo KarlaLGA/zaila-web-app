3b7113315c42c58b18831f50b8ba08f68ee02e76
KarlaLGA <KarlaLGA@users.noreply.github.com>
2020-02-05 22:23:55 -0800

    Update environment variable

--- src/components/Artwork/config.js
+++ src/components/Artwork/config.js
@@ -1,6 +1,6 @@
-import {runtimeEnv as env} from '@mars/heroku-js-runtime-env'
+import runtimeEnv from '@mars/heroku-js-runtime-env'
 
-
+let env = runtimeEnv();
 const config = {
     API_KEY: env.TRANSLATE_API_KEY
     //process.env.TRANSLATE_API_KEY
