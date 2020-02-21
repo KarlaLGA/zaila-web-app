628e988137f9765bf31332f5635b0fc487a9be97
KarlaLGA <KarlaLGA@users.noreply.github.com>
2020-02-21 09:18:30 -0800

    Username validator

--- src/components/LandingPage/LogIn.js
+++ src/components/LandingPage/LogIn.js
@@ -1,7 +1,58 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
+import React, {useState} from 'react';
+import {Link, useHistory} from 'react-router-dom'
 
 const LogIn = () => {
+
+    const history = useHistory();
+
+    const initialForm = {
+        username: '',
+        password: ''
+    }
+
+    const initialError = {
+        usernameError: '',
+        submitError: ''
+    }
+
+    const [logInForm,
+        setLogInForm] = useState(initialForm);
+    const [errorForm,
+        setErrorForm] = useState(initialError);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        let {username, password} = logInForm;
+
+        let errorMessage = {
+            usernameError: '',
+            submitError: ''
+        }
+
+        let submitUsername = username.trim();
+        let submitPassword = password.trim();
+
+        let formValidates = true;
+
+        if (submitUsername === "") {
+            errorMessage.usernameError = 'Please enter your username';
+            formValidates = false;
+        }
+
+        const submitLogIn = {
+            username: submitUsername,
+            password: submitPassword
+        };
+
+        if (formValidates) {
+            console.log(submitLogIn);
+            history.push("/dashboard");
+        }
+
+        setErrorForm(errorMessage);
+
+    }
     return (
         <div className="log-in">
             <div className="image">
@@ -9,12 +60,30 @@ const LogIn = () => {
             </div>
             <div className="log-in-form">
                 <p>Logo</p>
-                <form action="">
-                    <label htmlFor="username">Username
-                        <input type="text" name="username" id="username"/>
-                    </label>
+                <form action="" onSubmit={handleSubmit}>
+
+                    <p className="error-message">{errorForm.submitError}</p>
+
+                    <label htmlFor="username">Username</label>
+                    <div className="error-message">{errorForm.usernameError}</div>
+                    <input
+                        type="text"
+                        name="username"
+                        id="username"
+                        onChange={e => {
+                        setLogInForm({
+                            ...logInForm,
+                            username: e.target.value
+                        })
+                    }}/>
+
                     <label htmlFor="password">Password
-                        <input type="password" name="password" id="password"/>
+                        <input type="password" name="password" id="password" onChange={e => {
+                        setLogInForm({
+                            ...logInForm,
+                            password: e.target.value
+                        })
+                    }}/>
                     </label>
 
                     <button>Log In</button>
