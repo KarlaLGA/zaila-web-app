27fdaf17de09c8301dd5a62666e0a9acbaf70b1f
KarlaLGA <KarlaLGA@users.noreply.github.com>
2020-02-24 08:46:46 -0800

    Connect exhibition list to API

--- src/components/Exhibition/ListExhibitions/ListExhibition.js
+++ src/components/Exhibition/ListExhibitions/ListExhibition.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from "react";
 import {useDispatch} from "react-redux";
+import { get } from "services/zaila-api.js";
 
 import ExhibitionItem from "./ExhibitionItem"
 
@@ -7,30 +8,17 @@ const ListArtwork = () => {
     const dispatch = useDispatch();
     const [exhibitions,
         setExhibitions] = useState([]);
-
-    const data = [
-        {
-            exhibition: {
-                exhibitionId: 123,
-                museumId: 1,
-                name: "Egypt",
-                description: "Ancient Egyptian art refers art produced in ancient Egypt between the 31st century BC and the 4th century AD. It includes paintings, sculptures, drawings on papyrus, faience, jewelry, ivories, architecture, and other art media. It is also very conservative: the art style changed very little over time. Much of the surviving art comes from tombs and monuments, giving more insight into the Egyptians' belief of the afterlife.",
-                imageURL: "https://i.picsum.photos/id/116/400/200.jpg",
-                startDate: "2018-01-01T00:00:00.000Z",
-                endDate: "2020-05-31T00:00:00.000Z",
-                categoryId: 1,
-                completionBadgeId: 1,
-                completionXP: 10
-            }
-        }
-    ]
     
     useEffect(() => {
-        dispatch({type: "SET_EXHIBITION_LIST", payload: data});
-        dispatch({type: "EMPTY_EXHIBITION_DETAILS"});
-        setExhibitions(data);
+        get("exhibition")
+        .then(data => {
+            setExhibitions(data)
+        })
+        .catch(error => console.log(error));
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
+
+    console.log(exhibitions);
 
 
 return (
