15a6c9a8974401c0f8673495e0ece36c90034c4b
KarlaLGA <KarlaLGA@users.noreply.github.com>
2020-03-02 23:44:29 -0800

    Update ListArtworks component to match wireframe

--- src/components/Artwork/ListArtworks/ListArtwork.js
+++ src/components/Artwork/ListArtworks/ListArtwork.js
@@ -2,8 +2,6 @@ import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { get } from "services/zaila-api";
 import ArtworkItem from "./ArtworkItem";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 const ListArtwork = () => {
   const dispatch = useDispatch();
@@ -19,30 +17,11 @@ const ListArtwork = () => {
       .catch(error => {
         console.log(error);
       });
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-
-  const handleSearch = () => {
-    const search = document.getElementById("search-artwork").value;
-    console.log(search);
-    // TODO: ADD API CALL FOR SEARCH ARTWORK
-  };
 
   return (
     <div>
-      <div className="search-bar">
-        <label htmlFor="search-artwork">
-          <input
-            type="text"
-            name="search-artwork"
-            id="search-artwork"
-            placeholder="search by title or artist name"
-          />
-        </label>
-        <button onClick={handleSearch}>
-          <FontAwesomeIcon icon={faSearch} />
-        </button>
-      </div>
       {artworks.map(({ artwork }) => (
         <ArtworkItem key={artwork.artworkId} artwork={artwork} />
       ))}
--- src/views/screens/Artworks/ListOf.js
+++ src/views/screens/Artworks/ListOf.js
@@ -1,30 +1,34 @@
-import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { Link } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
 import { get } from "services/zaila-api";
 
-import ListArtwork from 'components/Artwork/ListArtworks/ListArtwork';
+import ListArtwork from "components/Artwork/ListArtworks/ListArtwork";
+import SearchBar from "components/Artwork/ListArtworks/SearchBar";
 
 const ListOf = () => {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
+  useEffect(() => {
+    get("exhibition").then(data => {
+      dispatch({ type: "SET_EXHIBITIONS", payload: data });
+    });
+  }, [dispatch]);
 
-    useEffect(() => {
-        get("exhibition")
-        .then(data => {
-          dispatch({type: "SET_EXHIBITIONS", payload: data})
-        })
-      }, [dispatch])
+  return (
+    <div className="list artwork-list">
+      <div className="section-header section-header-artworks">
+        <h2 className="section-title">Artworks</h2>
+        <SearchBar />
+      </div>
 
-    return (
-        <div className="list artwork-list">
+      <ListArtwork />
 
-            <ListArtwork/>
-
-            <Link to="/dashboard/artworks/create" className="add">Add</Link>
-            
-        </div>
-    )
-}
+      <Link to="/dashboard/artworks/create" className="add">
+        Add
+      </Link>
+    </div>
+  );
+};
 
 export default ListOf;
