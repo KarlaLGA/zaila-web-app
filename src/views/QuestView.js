8519661a5efb687eabc7315b95a28d92b744b45c
KarlaLGA <KarlaLGA@users.noreply.github.com>
2020-03-26 08:03:38 -0700

    Add placeholder content for Quests and Reports

--- src/views/QuestView.js
+++ src/views/QuestView.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const QuestView = () => {
-    return (
-        <div className="quest-view view">
-            <p>Quest view</p>
-        </div>
-    )
-}
+  return (
+    <div className="quest-view view">
+      <div className="section-header">
+        <h1>Quests</h1>
+      </div>
+      <p>Hang in there, this feature is coming soon!</p>
+    </div>
+  );
+};
 
 export default QuestView;
