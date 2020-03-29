c744477c494fdf406d7f08f7075d7c5ea9c585bc
KarlaLGA <KarlaLGA@users.noreply.github.com>
2020-03-28 17:22:02 -0700

    Update fetch for bluetooths on home view

--- src/views/LogInView.js
+++ src/views/LogInView.js
@@ -5,6 +5,8 @@ import NavBar from "../components/LandingPage/Navigation/NavBar";
 import Footer from "../components/LandingPage/Navigation/Footer";
 
 const LogInView = () => {
+  localStorage.removeItem("userData");
+
   return (
     <div className="log-in">
       <NavBar />
